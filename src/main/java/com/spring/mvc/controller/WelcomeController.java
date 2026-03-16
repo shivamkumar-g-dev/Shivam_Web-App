1d4929e83a7b9665960a41cd64abb2c52be9ffcf
shivamkumar-g-dev <83403644+shivamkumar-g-dev@users.noreply.github.com>
2026-03-16 15:20:45 +0530

    Initial commit

---
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -0,0 +1,27 @@
+package com.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/")
+	public String greeting() {
+		System.out.println("WelcomeController.greeting()");
+		return "index";
+	}
+
+	@GetMapping("/req-param")
+	public String createUser(@RequestParam(name = "user") String name, Model model) {
+		
+		model.addAttribute("user", name);
+
+		System.out.println("WelcomeController.greeting :" + name);
+
+		return "success";
+	}
+
+}
