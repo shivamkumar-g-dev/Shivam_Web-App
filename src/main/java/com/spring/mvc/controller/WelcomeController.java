408df6c764aa69f5fb5a7866c6168f7f4bc26ce2
shivamkumar-g-dev <83403644+shivamkumar-g-dev@users.noreply.github.com>
2026-03-19 00:54:51 +0530

    update

--- src/main/java/com/spring/mvc/controller/WelcomeController.java
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -2,15 +2,9 @@ package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class WelcomeController {
@@ -25,14 +19,6 @@ public class WelcomeController {
 
 	}
 	
-//	@GetMapping("/sign-up")
-//	public String createUser() {
-//		
-//		System.out.println("WelcomeController.greeting()");
-//		return "success";
-//		
-//
-//	}
 	
 	@GetMapping("/req-param")
 	public String createUser(@RequestParam(name = "user", defaultValue = "MY-DEFAULT-NAME") String name, Model model) {
@@ -44,6 +30,7 @@ public class WelcomeController {
 		return "success";
 	}
 
+
 	@GetMapping("/path-var/{id}")
 	public String pathVarExample(@PathVariable(name = "id") Long id, Model model) {
 
@@ -52,37 +39,6 @@ public class WelcomeController {
 		System.out.println("WelcomeController.Pathvar : " + id);
 
 		return "success";
-	}
-	
-//	@GetMapping("/getCookie")
-//	public String getCookieVal(@CookieValue(name = "JSESSIONID") String JSESSIONID, Model model) {
-//
-//		model.addAttribute("cookie", "Your JSESSIONID is : " + JSESSIONID);
-//
-//		System.out.println("JSESSIONID : " + JSESSIONID);
-//
-//		return "cookieVal";
-//	}
-	
-	@GetMapping("/getMyCookie")
-	public String getMyCookieVal(@CookieValue(name = "firstCookie" , defaultValue = "DefaultCookie") String firstCookie, Model model) {
-
-		model.addAttribute("firstCookie",  firstCookie);
-
-		System.out.println("getMyCookieVal : " + firstCookie);
-
-		return "cookieVal";
-	}
-
-	@GetMapping("/setCookie")
-	public String setCookieVal(HttpServletResponse response,Model model) {
-
-		Cookie cookie1 = new Cookie("firstCookie", "SensitiveInformation");
-		cookie1.setMaxAge(10);
-		response.addCookie(cookie1);
-		System.out.println("WelcomeController.setCookieVal()");
-
-		return "redirect:/getMyCookie";
 	}
 
 }
